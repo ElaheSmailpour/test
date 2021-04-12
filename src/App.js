d5ac1e2733c694dc8179cb5a9109eb9df44118f7
Elahesmailpour <eli_es87@yahoo.com>
2021-04-12 16:20:10 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [name, setName] = useState([]);
+
+	useEffect(() => {
+		async function response() {
+			let res = await Axios.get("/user");
+			let data = res.data;
+			console.log(data);
+			return data;
+		}
+		response().then((data) => {
+			setName(data);
+		});
+		// fetch("http://localhost:5000/user")
+		// 	.then((httpResponse) => {
+		// 		console.log(httpResponse);
+		// 		// httpResponse.json().then((antwortObjekt) => {
+		// 		// 	const leute = antwortObjekt;
+		// 		// 	setname(leute);
+		// 		// });
+		// 	})
+		// 	.catch((fehler) => {
+		// 		console.error(fehler);
+		// 	});
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Hallo...</p>
+				<ul>
+					{name.map((username, i) => {
+						return <li key={"name" + i}>{username.name}</li>;
+					})}
+				</ul>
+			</header>
+		</div>
+	);
 }
 
 export default App;
